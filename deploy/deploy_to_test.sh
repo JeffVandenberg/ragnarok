0d2fa15f50a40ebb022328d3bf071e9f8097ad02
JeffVandenberg <jeffvandenberg@gmail.com>
2018-01-15 21:38:28 -0800

    no interaction

--- deploy/deploy_to_test.sh
+++ deploy/deploy_to_test.sh
@@ -5,8 +5,8 @@ echo "echo $CIRCLE_BUILD_NUM > build_number" >> ./deploy_script
 echo "git pull" >> ./deploy_script
 echo "bin/cake migrations migrate" >> ./deploy_script
 echo "bin/cake orm_cache clear" >> ./deploy_script
-echo "php ~/tools/composer.phar self-update" >> ./deploy_script
-echo "php ~/tools/composer.phar install" >> ./deploy_script
+echo "php ~/tools/composer.phar -n self-update" >> ./deploy_script
+echo "php ~/tools/composer.phar -n install" >> ./deploy_script
 echo "rm webroot/chat/js/cache/compiled-*" >> ./deploy_script
 echo "cat webroot/chat/js/*.js > webroot/chat/js/cache/compiled-${CIRCLE_BUILD_NUM}.js" >> ./deploy_script
 
