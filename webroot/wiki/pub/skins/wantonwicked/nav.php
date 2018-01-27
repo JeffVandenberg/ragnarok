4354f5256876b5e285b224ea54abe4cb04c5835c
Jeff Vandenberg <jeffvandenberg@gmail.com>
2018-01-26 20:20:42 -0800

    add da wiki

---
+++ webroot/wiki/pub/skins/wantonwicked/nav.php
@@ -0,0 +1,106 @@
+<ul class='menu' id='main-menu'>
+    <li><a href="/" target="_top"><span>Home</span></a></li>
+    <li><a href="#"><span>The Setting</span></a>
+        <ul>
+            <li><a href="/wiki/City/City"><span>The City</span></a></li>
+            <li><a href="/wiki/Changeling/Changeling"><span>Changeling</span></a>
+                <ul>
+                    <li><a href="/wiki/Changeling/HouseRules"><span>House Rules</span></a></li>
+                    <li><a href="/wiki/Changeling/PlayerGuide"><span>Player Guide</span></a></li>
+                    <li><a href="/characters/cast/Changeling"><span>Cast</span></a></li>
+                </ul>
+            </li>
+            <li><a href="/wiki/Geist/Geist"><span>Geist</span></a>
+                <ul>
+                    <li><a href="/wiki/Geist/HouseRules"><span>House Rules</span></a></li>
+                    <li><a href="/wiki/Geist/PlayerGuide"><span>Player Guide</span></a></li>
+                    <li><a href="/characters/cast/Geist"><span>Cast</span></a></li>
+                </ul>
+            </li>
+            <li><a href="/wiki/Mage/Mage"><span>Mage</span></a>
+                <ul>
+                    <li><a href="/wiki/Mage/HouseRules"><span>House Rules</span></a></li>
+                    <li><a href="/wiki/Mage/PlayerGuide"><span>Player Guide</span></a></li>
+                    <li><a href="/characters/cast/Mage"><span>Cast</span></a></li>
+                </ul>
+            </li>
+            <li><a href="/wiki/Mortal/Mortal"><span>Mortal</span></a>
+                <ul>
+                    <li><a href="/wiki/Mortal/HouseRules"><span>House Rules</span></a></li>
+                    <li><a href="/wiki/Mortal/PlayerGuide"><span>Player Guide</span></a></li>
+                    <li><a href="/characters/cast/Mortal"><span>Cast</span></a></li>
+                </ul>
+            </li>
+            <li><a href="/wiki/Vampire/Vampire"><span>Vampire</span></a>
+                <ul>
+                    <li><a href="/wiki/Vampire/HouseRules"><span>House Rules</span></a></li>
+                    <li><a href="/wiki/Vampire/PlayerGuide"><span>Player Guide</span></a></li>
+                    <li><a href="/characters/cast/Vampire"><span>Cast</span></a></li>
+                </ul>
+            </li>
+            <li><a href="/wiki/Werewolf/Werewolf"><span>Werewolf</span></a>
+                <ul>
+                    <li><a href="/wiki/Werewolf/HouseRules"><span>House Rules</span></a></li>
+                    <li><a href="/wiki/Werewolf/PlayerGuide"><span>Player Guide</span></a></li>
+                    <li><a href="/characters/cast/Werewolf"><span>Cast</span></a></li>
+                </ul>
+            </li>
+            <li><a href="#"><span>Crossover Sub-venues</span></a>
+                <ul>
+                    <li><a href="/wiki/Whitefield/Whitefield"><span>Whitefield</span></a>
+                        <ul>
+                            <li><a href="/wiki/Whitefield/HouseRules"><span>House Rules</span></a></li>
+                            <li><a href="/wiki/Whitefield/PlayerGuide"><span>Player Guide</span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="/characters/cast"><span>The Cast</span></a></li>
+        </ul>
+    </li>
+    <li><a href="#"><span>Game Guide</span></a>
+        <ul>
+            <li><a href="/wiki/GameRef/HouseRules"><span>House Rules</span></a>
+                <ul>
+                    <li><a href="/wiki/GameRef/CrossoverErrata"><span>Crossover Errata</span></a></li>
+                </ul>
+            </li>
+            <li><a href="/wiki/GameRef/CharacterCreation"><span>Character Creation</span></a>
+                <ul>
+                    <li><a href="/wiki/GameRef/SanctioningGuide"><span>Sanctioning Checklist</span></a></li>
+                    <li><a href="/wiki/GameRef/BookPolicy"><span>Book Policy</span></a></li>
+                </ul>
+            </li>
+            <li><a href="/wiki/GameRef/ExperienceGuide"><span>Experience Guide</span></a></li>
+            <li><a href="/wiki/GameRef/PoliciesandPractices"><span>Policies and Practices</span></a>
+                <ul>
+                    <li><a href="/wiki/GameRef/CrossoverPolicy"><span>Crossover Policy</span></a></li>
+                </ul>
+            </li>
+        </ul>
+    </li>
+    <li><a href="#"><span>Tools</span></a>
+        <ul>
+            <li><a href="/wiki/GameRef/Help"><span>Help</span></a>
+                <ul>
+                    <li><a href="/wiki/GameRef/RequestSystemHelp"><span>Request System</span></a></li>
+                    <li><a href="/wiki/GameRef/ChatHelp"><span>Chat Interface</span></a></li>
+                </ul>
+            </li>
+            <li><a href="support.php"><span>Site Supporter</span></a></li>
+        </ul>
+    </li>
+    <li><a href="/forum/index.php"><span>Forums</span></a></li>
+    <li><a href="#"><span>Site Info</span></a>
+        <ul>
+            <li><a href="/staff"><span>The Team</span></a></li>
+            <li><a href="/wiki/GameRef/CodeOfConduct"><span>Code of Conduct</span></a>
+                <ul>
+                    <li><a href="/wiki/GameRef/PersonalInformation"><span>Personal Information</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="/wiki/GameRef/Disclaimer"><span>Disclaimer</span></a></li>
+        </ul>
+    </li>
+</ul>
